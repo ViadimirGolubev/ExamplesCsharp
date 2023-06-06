25f50b18f2d930847b4cbcf138d8d469db4ba4b7
ViadimirGolubev <golubevvlvl@mail.ru>
2023-06-06 15:41:17 +0300

    add random

--- Еxample003_Sum/Program.cs
+++ Еxample003_Sum/Program.cs
@@ -4,7 +4,26 @@
 // int numberВ = 5; 
 // Console.WriteLine(numberA + numberВ );
 
-int numberA = 31;
-int numberВ = 5;
-int result = numberA + numberВ; 
+// int numberA = 31;
+// int numberВ = 5;
+// int result = numberA + numberВ; 
+// Console.WriteLine(result);
+
+// int numberA = new Random().Next(1,10); 
+// int numberB = new Random().Next(1,10); 
+// int result = numberA + numberB; 
+// Console.WriteLine(result);
+
+// int numberA = new Random().Next(1,10);
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1,10); 
+// Console.WriteLine(numberВ);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberC = new Random().Next(1,10);
+Console.WriteLine(numberC);
+int result = numberA + numberC; 
 Console.WriteLine(result);
